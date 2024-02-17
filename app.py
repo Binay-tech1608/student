da49946879f7cb251d290e222cdb3ba79deb8fda
binaytech-1608 <binaymann86@gmail.com>
2024-02-17 21:44:09 +0530

    modify app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from aiohttp import request
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,6 +7,19 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/user/<username>")
+def show_user(username):
+    return f"User: {username}"
+
+
+@app.route("/user/<int:user_id>/edit", methods=["GET", "POST"])
+def edit_user(user_id):
+    if request.method == "POST":
+        # update user in database...
+        pass
+    return render_template("edit_user.html")
 
 
 if __name__ == "__main__":
